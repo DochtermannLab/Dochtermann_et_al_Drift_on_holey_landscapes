567ba72df901f9300f28a00ed95a1e5c5e3c5192
Ned Dochtermann <47366531+DochtermannLab@users.noreply.github.com>
2020-11-12 16:41:21 -0600

    Update Wright_vs_Holey(SimulationCode).R

--- Wright_vs_Holey(SimulationCode).R
+++ Wright_vs_Holey(SimulationCode).R
@@ -1,13 +1,12 @@
-#### Run Conditions ####
-N=7500
-n.traits=10
-prop=.5
-h2=.8
-n.gen=100
-p=0.5
-nsim=250
+#### General Run Conditions ####
+N=7500 #Number of individuals
+n.traits=10 #Number of traits
+h2=.8 #heritability
+n.gen=100 #Number of generations
+nsim=250 #Number of simulations
 
 #### IVC Model (Wright LS) ####
+prop=0.5 #proportion selected
 eigen.stor.Wright<-matrix(NA,nsim,n.traits)
 #Fitness landscape/ISS
 for(j in 1:nsim){
@@ -23,6 +22,7 @@ Sigma.G=diag(n.traits)
 Sigma.E=diag(n.traits)
 
 eigen.stor<-matrix(NA,n.gen,n.traits)
+#fit.stor <- matrix(NA,n.gen,2)
 
 #main model
 for (i in 1:n.gen){
@@ -31,6 +31,8 @@ for (i in 1:n.gen){
   E<-mvrnorm(N,mu=mu.E,Sigma=Sigma.E)
   P<-h2*G+(1-h2)*E
   fit<-apply(P,1,fitness.AL.Lande1980,omega=Wright.ISS,optimum=optimum)
+  #fit.stor[i,1] <- mean(fit)
+  #fit.stor[i,2] <- sd(fit)
   pop<-cbind(fit,P,G,E)
   surv.pop<-trunc.select(pop,prop=prop)
   
@@ -42,7 +44,8 @@ eigen.stor.Wright[j,]<-eigen.stor[n.gen,]
 }
 Wright.12<-eigen.stor.Wright[,1]/eigen.stor.Wright[,2]
 
-#### IVC Model (Holey LS) ####
+#### IVC Model (Holey LS) P = 0.5 ####
+p=0.5 #holey landscape proportion parameter
 
 eigen.stor.Holey<-matrix(NA,nsim,n.traits)
 
@@ -68,7 +71,7 @@ for (i in 1:n.gen){
   eigen.stor[i,]<-c(eigen(cov(G))$values)
   E<-mvrnorm(N,mu=mu.E,Sigma=Sigma.E)
   P<-h2*G+(1-h2)*E
-  P2<-make.binary(P)#convert P to threshold 0 or 1
+  P2<-make.binary(P)#convert P to 0 or 1
   fit<-apply(P2,1,fitness.HL,mat=HLS.t,HLS.fit=HLS.fit)
   pop<-cbind(fit,P,G,E)
   surv.pop<-pop[which(pop[,1]==1),]
@@ -157,20 +160,55 @@ Holey.12.8<-eigen.stor.Holey8[,1]/eigen.stor.Holey8[,2]
 #save.image("C:/Users/Dochtermann/Dropbox/Working/Projects/Genetics.Exeter/IVCsimulation2.RData")
 
 
+#### IVC Model (Drift only) ####
+prop=0.5 #proportion surviving (same as Wright LS sims)
+N=7500
+n.traits=10
+n.gen=100
+nsim=250
+
+eigen.stor.drift<-matrix(NA,nsim,n.traits)
+#Fitness landscape/ISS
+for(j in 1:nsim){
+  
+  #starting values
+  mu.G=rep(0,n.traits)
+  Sigma.G=diag(n.traits)
+  
+  eigen.stor<-matrix(NA,n.gen,n.traits)
+  
+  #main model
+  for (i in 1:n.gen){
+    G<-mvrnorm(N,mu=mu.G,Sigma=Sigma.G)
+    eigen.stor[i,]<-c(eigen(cov(G))$values)
+    pop<-G
+    surv.pop <- pop[sample(N,N*prop,replace=F),] #randomly sample population for how survives
+    Sigma.G<-cov(surv.pop[,c(1:n.traits)])
+    mu.G<-colMeans(surv.pop[,c(1:n.traits)])
+  }
+  eigen.stor.drift[j,]<-eigen.stor[n.gen,]
+}
+hist(eigen.stor.drift[,2]/eigen.stor.drift[,1])
+
 #### Model Summary ####
 
 eigen.stor.Wright.std1<-eigen.stor.Wright/apply(eigen.stor.Wright,1,max)
 eigen.stor.Holey.std1<-eigen.stor.Holey/apply(eigen.stor.Holey,1,max)
 eigen.stor.Holey2.std1<-eigen.stor.Holey2/apply(eigen.stor.Holey2,1,max)
 eigen.stor.Holey8.std1<-eigen.stor.Holey8/apply(eigen.stor.Holey8,1,max)
+eigen.stor.Drift.std1<-eigen.stor.drift/apply(eigen.stor.drift,1,max)
 
 comb.ls2<-as.data.frame(rbind(matrix(eigen.stor.Wright.std1[,2],ncol=1),
+                              matrix(eigen.stor.Drift.std1[,2],ncol=1),
                               matrix(eigen.stor.Holey2.std1[,2],ncol=1),
                              matrix(eigen.stor.Holey.std1[,2],ncol=1),
                              matrix(eigen.stor.Holey8.std1[,2],ncol=1)))
 names(comb.ls2)<-c("EigenRatio")
-comb.ls2$LS<-c(rep("Wright",nsim),rep("Holey p = 0.2",nsim),rep("Holey p = 0.5",nsim),
-              rep("Holey p = 0.8",nsim))
+comb.ls2$LS<-c(rep("Wright",nsim),rep("Drift",nsim),rep("Holey p = 0.2",nsim),rep("Holey p = 0.5",nsim),
+               rep("Holey p = 0.8",nsim))
+comb.ls2$LS <- as.factor(comb.ls2$LS)
+comb.ls2$LS <- factor(comb.ls2$LS,levels(comb.ls2$LS)[c(2,3,4,5,1)])
+
 
 sum.ls2<-cbind(tapply(comb.ls2$EigenRatio, comb.ls2$LS, mean),
               tapply(comb.ls2$EigenRatio, comb.ls2$LS, quantile, probs=0.025),
@@ -178,75 +216,6 @@ sum.ls2<-cbind(tapply(comb.ls2$EigenRatio, comb.ls2$LS, mean),
 summary(aov(EigenRatio~LS,data=comb.ls2))
 TukeyHSD(aov(EigenRatio~LS,data=comb.ls2))
 
-nd.Wright<-rowSums(eigen.stor.Wright.std1)
-nd.Holey5<-rowSums(eigen.stor.Holey.std1)
-nd.Holey2<-rowSums(eigen.stor.Holey2.std1)
-nd.Holey8<-rowSums(eigen.stor.Holey8.std1)
-
-
-nd.all<-as.data.frame(rbind(matrix(nd.Wright,ncol=1),
-                            matrix(nd.Holey2,ncol=1),
-                            matrix(nd.Holey5,ncol=1),
-                            matrix(nd.Holey8,ncol=1)))
-names(nd.all)<-c("nd")
-nd.all$LS<-c(rep("Wright",nsim),rep("Holey p = 0.2",nsim),rep("Holey p = 0.5",nsim),
-             rep("Holey p = 0.8",nsim))
-
-sum.nd<-cbind(tapply(nd.all$nd, comb.ls2$LS, mean),
-              tapply(nd.all$nd, comb.ls2$LS, quantile, probs=0.025),
-              tapply(nd.all$nd, comb.ls2$LS, quantile, probs=0.975))
-summary(aov(nd~LS,data=nd.all))
-TukeyHSD(aov(nd~LS,data=nd.all))
-
-
-
-#### Summary plot ####
-
-nd.Wright<-rowSums(eigen.stor.Wright.std1)
-nd.Holey5<-rowSums(eigen.stor.Holey.std1)
-nd.Holey2<-rowSums(eigen.stor.Holey2.std1)
-nd.Holey8<-rowSums(eigen.stor.Holey8.std1)
-
-
-nd.all<-as.data.frame(rbind(matrix(nd.Wright,ncol=1),
-                            matrix(nd.Holey2,ncol=1),
-                            matrix(nd.Holey5,ncol=1),
-                            matrix(nd.Holey8,ncol=1)))
-names(nd.all)<-c("nd")
-nd.all$LS<-c(rep("Wright",nsim),rep("Holey p = 0.2",nsim),rep("Holey p = 0.5",nsim),
-             rep("Holey p = 0.8",nsim))
-
-sum.nd<-cbind(tapply(nd.all$nd, comb.ls2$LS, mean),
-              tapply(nd.all$nd, comb.ls2$LS, quantile, probs=0.025),
-              tapply(nd.all$nd, comb.ls2$LS, quantile, probs=0.975))
-summary(aov(nd~LS,data=nd.all))
-TukeyHSD(aov(nd~LS,data=nd.all))
-
-
-#tiff('summary2.tiff',width=16,height=16,units='in',res=600)
-par(mfrow=c(2,1),pty='s',mar=c(6,7.5,.5,.5))
-
-boxplot(EigenRatio~LS,data=comb.ls2,xlab=" ",ylab=" ",xaxt="n",ylim=c(0,1.05),cex.axis=1.5)
-mtext(expression(paste(frac(lambda[2],lambda[1]))),2,
-      line=3,las=2,cex=2.5)
-min.xaxis<-c("Holey\n(p = 0.2)",
-             "Holey\n(p = 0.5)",
-             "Holey\n(p = 0.8)",
-             "Wrightian")
-axis(1, at=c(1:4), line=1,labels=min.xaxis,lty=0,cex.axis=1.5)
-text( c(1:4) , 1.04,c("A","A","A","B"),cex=1.25)
-
-boxplot(nd~LS,data=nd.all,xlab=" ",xaxt="n",ylim=c(0,5.2),ylab="Effective Number \nof Dimensions",
-        cex.lab=2,cex.axis=1.5)
-min.xaxis<-c("Holey\n(p = 0.2)",
-             "Holey\n(p = 0.5)",
-             "Holey\n(p = 0.8)",
-             "Wrightian")
-axis(1, at=c(1:4), line=1,labels=min.xaxis,lty=0,cex.axis=1.5)
-
-text( c(1:4) , 5.1,c("A","B","B","C"),cex=1.25)
-
-#dev.off()
 
 #### 2 x 2 plot ####
 #tiff('2by2.tiff',width=8,height=8,units='in',res=600)
@@ -302,4 +271,5 @@ points(y=colMeans(eigen.stor.Wright/apply(eigen.stor.Wright,1,max)),
        x=c(1:10),cex=2.5,lwd=1.5,pch=21,bg="white")
 #dev.off()
 #### End ####
-#save.image("C:/Users/Dochtermann/Dropbox/Working/Projects/Genetics.Exeter/IVCsimulation3.RData")
+#save.image("C:/Users/Ned Dochtermann/Dropbox/Working/Projects/HoleyLandscapes/IVCsimulation4.RData")
+
